f678394e86f51adf1f1986e539d4979775b96bde
James Berger <jberger@splunk.com>
2016-09-28 13:46:15 -0500

    adding class 5 files

---
+++ Class_5_Bash_scripting_2/interactive.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+clear
+
+echo "Type the name of the file you'd like me to analyze and press enter."
+
+read MYFILE
+
+echo -e "The name of your file is $MYFILE.\n"
+
+echo "Type out the name of the person to your left and the person to your right, separated by a space and press enter."
+
+read PERSON_ON_LEFT PERSON_ON_RIGHT
+
+echo -e "The person on your left is $PERSON_ON_LEFT and the person on your right is $PERSON_ON_RIGHT \n"
+
+echo "Press enter to exit."
+
+read PLACEHOLDER
+
+echo "Exiting..."
+
+clear
